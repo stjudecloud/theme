abe2d173eb21bc28873f5ab262a9aa18695d816b
St. Jude Cloud <support@stjude.cloud>
2020-02-28 22:37:35 +0000

    Deployed from Update build.sh 70706a59dec6fb5986085d33d61495494df657d0

--- branches/typography-update/js/navbar.js
+++
@@ -1,9 +0,0 @@
-// Only required for secondary navigation
-$('.secondary-nav .nav-link').on('click', function(e) {
-  // Bootstrap bug? https://github.com/twbs/bootstrap/issues/19374
-  e.preventDefault();
-  $(this).tab('show');
-  var clickedTab = $(this);
-  $('.secondary-nav .nav-link').removeClass('active');
-  clickedTab.addClass('active');
-});
