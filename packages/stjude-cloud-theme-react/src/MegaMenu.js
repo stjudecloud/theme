a75635d43f283625276dc2bcfc3fbcfda36b6979
Andrew Swistak <andrew@andrewswistak.com>
2020-02-28 12:13:21 -0600

    Create MegaMenu subcomponent from Navbar

---
+++ packages/stjude-cloud-theme-react/src/MegaMenu.js
@@ -0,0 +1,147 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Container, Row, Col } from "react-bootstrap";
+
+import FeaturedImage from "@stjudecloud/theme/dist/images/featured-study.jpg";
+import GenomicsPlatformIcon from "@stjudecloud/theme/dist/images/icon-genomics-platform.png";
+import PeCanIcon from "@stjudecloud/theme/dist/images/icon-pecan.png";
+import VizCommIcon from "@stjudecloud/theme/dist/images/icon-viz.png";
+
+function MegaMenu() {
+  return (
+    <Container className="mega-menu-container">
+      <Row>
+        <Col md={{ span: 3, order: 2 }} className="mega-dropdown-col">
+          <div className="mega-menu-title">Research Domains</div>
+          <ul className="list-unstyled">
+            <li>
+              <a href="https://stjude.cloud/pediatric-cancer.html">
+                Pediatric Cancer
+              </a>
+            </li>
+            <li>
+              <a href="https://stjude.cloud/cancer-survivorship.html">
+                Cancer Survivorship
+              </a>
+            </li>
+            <li>
+              <a href="https://stjude.cloud/noncancerous-diseases.html">
+                Non-cancerous Diseases
+              </a>
+            </li>
+          </ul>
+        </Col>
+        <Col md={{ span: 3, order: 3 }} className="mega-dropdown-col">
+          <div className="mega-menu-title">Apps</div>
+          <ul className="list-unstyled">
+            <li>
+              <a href="https://platform.stjude.cloud" className="app-title">
+                <img
+                  className="app-title__icon"
+                  src={GenomicsPlatformIcon}
+                  alt="Genomics Platform Icon"
+                />
+                <span>
+                  Genomics Platform
+                  <div className="genomics-platform-subtext">
+                    Data &amp; Tools
+                  </div>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="https://pecan.stjude.cloud/" className="app-title">
+                <img
+                  className="app-title__icon"
+                  src={PeCanIcon}
+                  alt="PeCan Icon"
+                />
+                <span>PeCan</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://viz.stjude.cloud" className="app-title">
+                <img
+                  className="app-title__icon"
+                  src={VizCommIcon}
+                  alt="Vizualization Community Icon"
+                />
+                <span>Visualization Community</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://stjude.cloud/apps.html" className="text-primary">
+                View All Apps
+              </a>
+            </li>
+          </ul>
+        </Col>
+        <Col md={{ span: 3, order: 1 }} className="mega-dropdown-col d-flex">
+          <div className="featured-study">
+            <a href="https://stjude.cloud/studies/clinical-genomics.html">
+              <img
+                className="featured-study__image"
+                src={FeaturedImage}
+                alt="Featured Study Image"
+              />
+            </a>
+            <div>
+              <div className="featured-study__title">Featured Study</div>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="https://stjude.cloud/studies/clinical-genomics.html">
+                    Real-time Clinical Genomics
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://stjude.cloud/studies.html"
+                    className="text-primary"
+                  >
+                    View All Studies
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Col>
+        <Col md={{ span: 3, order: 4 }} className="mega-dropdown-col">
+          <div className="mega-menu-title">Support</div>
+          <ul className="list-unstyled">
+            <li>
+              <a
+                href="https://www.stjude.cloud/docs/"
+                target="_blank"
+                rel="noopener"
+              >
+                Help Guides
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.stjude.cloud/docs/faq/"
+                target="_blank"
+                rel="noopener"
+              >
+                FAQs
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hospital.stjude.org/apps/forms/fb/st-jude-cloud-contact/"
+                target="_blank"
+                rel="noopener"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+MegaMenu.displayName = "St. Jude Cloud Navbar MegaMenu";
+
+export default MegaMenu;
