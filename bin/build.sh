4d69368dfe3fecc567e9e8776c221d80dcdf3cc7
Josh Burnwatt <josh.burnwatt@stjude.org>
2020-02-28 15:47:27 -0600

    Deploy branches to gh-pages on push

---
+++ bin/build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -e
+
+function build() {
+  _BRANCH=$1;
+  _DIR=$2;
+
+  mkdir -p "${_DIR}"
+
+  echo "  [*] Building ${_BRANCH} to ${_DIR}"
+  git checkout "${_BRANCH}"
+
+  npm install
+  npm run css-compile
+  
+  cp -p -R guide/* "${_DIR}"
+  cp -p -R dist/images/* "${_DIR}/images"
+  cp -p -R dist/fonts/* "${_DIR}/fonts"
+}
+
+# Stash starting branch name
+STARTING_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
+
+# Create build directory
+BUILD_DIR="./build"
+mkdir -p "$BUILD_DIR"
+
+# Get branch list
+git fetch --all
+BRANCHES=()
+while IFS= read -r line; do
+    BRANCHES+=( "$line" )
+done < <( git branch --list --all | sed 's,\*,,g' | xargs -n1 | grep "remotes/origin" | sed 's,remotes/origin/,,g' | sort | uniq )
+
+# Loop through branches, build and copy each to output dir
+echo "== Build Branches =="
+for CURRENT_BRANCH in "${BRANCHES[@]}"; do 
+  BRANCH_DIR="${BUILD_DIR}/branches/${CURRENT_BRANCH}"
+  if [[ "${CURRENT_BRANCH}" == "master" ]]; then
+    BRANCH_DIR="${BUILD_DIR}/"
+  fi
+  build "${CURRENT_BRANCH}" "${BRANCH_DIR}"
+done
+
+git checkout "${STARTING_BRANCH_NAME}"
