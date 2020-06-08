3756236a2051558b7efc9852677c2e5cc60e0d3c
Josh Burnwatt <josh.burnwatt@stjude.org>
2020-06-08 16:06:57 -0500

    fix: build script

--- bin/build.sh
+++ bin/build.sh
@@ -2,13 +2,9 @@
 set -e
 
 function build() {
-  _BRANCH=$1;
-  _DIR=$2;
+  _DIR=$1;
 
   mkdir -p "${_DIR}"
-
-  echo "  [*] Building ${_BRANCH} to ${_DIR}"
-  git checkout -f "${_BRANCH}"
 
   npm install
   npm run css-compile
@@ -25,21 +21,4 @@ STARTING_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
 BUILD_DIR="./build"
 mkdir -p "$BUILD_DIR"
 
-# Get branch list
-git fetch --all
-BRANCHES=()
-while IFS= read -r line; do
-    BRANCHES+=( "$line" )
-done < <( git branch --list --all | sed 's,\*,,g' | xargs -n1 | grep "remotes/origin" | sed 's,remotes/origin/,,g' | sort | uniq | grep -v 'gh-pages')
-
-# Loop through branches, build and copy each to output dir
-echo "== Build Branches =="
-for CURRENT_BRANCH in "${BRANCHES[@]}"; do 
-  BRANCH_DIR="${BUILD_DIR}/branch/${CURRENT_BRANCH}"
-  if [[ "${CURRENT_BRANCH}" == "master" ]]; then
-    BRANCH_DIR="${BUILD_DIR}/"
-  fi
-  build "${CURRENT_BRANCH}" "${BRANCH_DIR}"
-done
-
-git checkout -f "${STARTING_BRANCH_NAME}"
+build "${BUILD_DIR}"
