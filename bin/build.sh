ffe33840c3367473d191985389bb21005f077a85
Josh Burnwatt <josh.burnwatt@stjude.org>
2020-06-17 21:29:30 -0500

    chore: PR fixes

--- bin/build.sh
+++ bin/build.sh
@@ -9,16 +9,14 @@ function build() {
   npm install
   npm run css-compile
 
-  cp -p -R guide/* "${_DIR}"
-  cp -p -R dist/images/* "${_DIR}/images"
-  cp -p -R dist/fonts "${_DIR}/fonts"
+  cp -p -R ../guide/* "${_DIR}"
+  cp -p -R ../dist/images/* "${_DIR}/images"
+  cp -p -R ../dist/fonts "${_DIR}/fonts"
 }
 
-# Stash starting branch name
-STARTING_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
 
 # Create build directory
-BUILD_DIR="./build"
+BUILD_DIR="../build"
 mkdir -p "$BUILD_DIR"
 
 build "${BUILD_DIR}"
