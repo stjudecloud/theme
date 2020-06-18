a9c59b731196be9967ffcae268e08c272be25662
Josh Burnwatt <josh.burnwatt@stjude.org>
2020-06-17 21:37:59 -0500

    chore: refactor build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-function build() {
-  _DIR=$1;
+mkdir -p "../build"
 
-  mkdir -p "${_DIR}"
+npm install
+npm run css-compile
 
-  npm install
-  npm run css-compile
-
-  cp -p -R ../guide/* "${_DIR}"
-  cp -p -R ../dist/images/* "${_DIR}/images"
-  cp -p -R ../dist/fonts "${_DIR}/fonts"
-}
-
-
-# Create build directory
-BUILD_DIR="../build"
-mkdir -p "$BUILD_DIR"
-
-build "${BUILD_DIR}"
+cp -p -R ../guide/* "../build"
+cp -p -R ../dist/images/* "../build/images"
+cp -p -R ../dist/fonts "../build/fonts"
