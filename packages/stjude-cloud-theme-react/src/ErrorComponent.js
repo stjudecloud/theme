099ff5a131f3a7bf43f5fd714c57eaad2abc0093
Stephanie Wiggins <stephawiggins@gmail.com>
2020-08-14 14:37:52 -0500

    Updates 404 component and corresponding React component

--- packages/stjude-cloud-theme-react/src/ErrorComponent.js
+++ packages/stjude-cloud-theme-react/src/ErrorComponent.js
@@ -1,61 +1,39 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Button, Image } from "react-bootstrap";
-import NotFoundImage from "@stjudecloud/theme/dist/images/404.png";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 
-function ErrorComponent({ variant }) {
-  return (
-    <div className="d-flex">
-      <div className="sjc-error">
-        {variant === "notFound" && (
-          <Image className="error-image" src={NotFoundImage} />
-        )}
-
-        <div className="error-list-container">
-          <h2 className="error-header">
-            {(variant === "notFound" && "Page Not Found") ||
-              "Internal Server Error"}
-          </h2>
-          <p className="error-subheader">
-            {variant === "internalServerError" && (
-              <>
-                Something went wrong! You could try refreshing the page or
-                looking elsewhere.
-                <br />
-                <br />
-              </>
-            )}
-            Here are some helpful links instead.
-          </p>
-          <div className="error-links-list">
-            <a href="https://www.stjude.cloud/">St. Jude Cloud Home</a>
-            <br />
-            <a href="https://stjude.cloud/docs/">Documentation</a>
-            <br />
-            <a
-              href="https://stjude.cloud/contact"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Contact Us
-            </a>
-          </div>
+const ErrorComponent = ({ variant }) => (
+  <div className="not-found">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          {variant === 'internalServerError' && (
+            <>
+              <h2>500</h2>
+              <h4>Something went wrong. You could try refreshing the page or looking elsewhere.</h4>
+            </>
+          )}
+          {variant === 'notFound' && (
+            <>
+              <h2>404</h2>
+              <h4>The page you are looking for is not here.</h4>
+            </>
+          )}
+          <Button variant="primary" href="/">
+            Back to Home
+          </Button>
         </div>
-
-        <Button variant="primary" className="w-100 mt-3" href="/">
-          Take me to the homepage
-        </Button>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export const defaultProps = {
-  variant: "internalServerError"
+  variant: 'internalServerError',
 };
 
 export const propTypes = {
-  variant: PropTypes.oneOf(["notFound", "internalServerError"]).isRequired
+  variant: PropTypes.oneOf(['notFound', 'internalServerError']).isRequired,
 };
 
 ErrorComponent.defaultProps = defaultProps;
