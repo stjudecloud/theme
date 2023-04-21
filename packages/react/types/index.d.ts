668f26b19a4d2c8f5eab8ba404be5c2cc5963db6
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-04-21 11:15:04 -0500

    add mega menu interface

--- packages/react/types/index.d.ts
+++ packages/react/types/index.d.ts
@@ -1,3 +1,4 @@
-export { default as Navbar } from './components/Navbar';
-export { default as Footer } from './components/Footer';
-export { default as ErrorComponent } from './components/ErrorComponent';
+export { default as Navbar } from "./components/Navbar";
+export { default as Footer } from "./components/Footer";
+export { default as MegaMenu } from "./components/MegaMenu";
+export { default as ErrorComponent } from "./components/ErrorComponent";
